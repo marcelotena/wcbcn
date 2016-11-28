76c94bea307600dfae5a0f7ff62fff0fd23cc713
Marcelo Tena <marcelotena@fastandyours.com>
2016-11-28 16:38:03 +0100

    CSS adjustments.

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 <footer id="colophon" class="site-footer" role="contentinfo">
     <div class="site-info">
-        <p class="copyright-footer">Demo WordCamp Santander 2016</p>
+        <p class="copyright-footer">Demo WordCamp Barcelona 2016</p>
         <a href="https://twitter.com/marcelotena" class="footer-link"><i class="material-icons md-18">person_add</i> @marcelotena</a>
     </div><!-- .site-info -->
 </footer><!-- #colophon .site-footer -->
